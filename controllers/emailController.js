b86dd5aee892d41623b3d5c9b1b5a63a725e6623
U-riani <sandro.papi@yahoo.com>
2024-07-22 15:17:41 +0400

    fff

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const nodemailer = require("nodemailer");
 
 const emailController = async (req, res) => {
@@ -8,14 +7,14 @@ const emailController = async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.COMPANY_EMAIL,
-      pass: process.env.EMAIL_PASS, 
+      user: "sandropapiashvili@gmail.com",
+      pass: "gjpr lqtk yxdk pmsu", // Ensure this is correct
     },
   });
 
   const mailOptions = {
     from: email,
-    to: process.env.COMPANY_EMAIL,
+    to: "sandropapiashvili@gmail.com",
     subject: "Contact Form Submission",
     text: `Name: ${name}\nCompany Name: ${companyName}\nEmail: ${email}\nPhone Number: ${countryCode} ${phoneNumber}\nComments: ${comments}`,
   };
@@ -23,7 +22,7 @@ const emailController = async (req, res) => {
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error("Error sending email:", error);
-      res.status(500).send("Error sending email", error); 
+      res.status(500).send("Error sending email"); // Correct way to send status code and message
     } else {
       console.log("Email sent:", info.response);
       res.status(200).send("Email sent successfully"); // Correct way to send status code and message
