90b6aa6de089b6e2fb0e9d9919dfdb7996d3b212
U-riani <sandro.papi@yahoo.com>
2024-08-29 22:55:51 +0400

    lets get back

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -16,7 +16,7 @@ const emailController = async (req, res) => {
   const mailOptions = {
     from: email,
     to: process.env.COMPANY_EMAIL,
-    subject: "NEW MAIL FROM WEBSITE",
+    subject: "Contact Form Submission",
     text: `Name: ${name}\nCompany Name: ${companyName}\nEmail: ${email}\nPhone Number: ${countryCode} ${phoneNumber}\nComments: ${comments}`,
   };
 
