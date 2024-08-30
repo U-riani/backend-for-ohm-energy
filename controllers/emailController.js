4c42cbb68b847d476bfbd4e99b46b1856b63d4f1
U-riani <sandro.papi@yahoo.com>
2024-08-30 17:54:43 +0400

    dfdsfdsfsdffsdf

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -9,8 +9,8 @@ const emailController = async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: EMAIL_PASSEMAIL_PASS, // Ensure this is correct
+      user: process.env.COMPANY_EMAIL,
+      pass: process.env.EMAIL_PASS, // Ensure this is correct
     },
   });
 
