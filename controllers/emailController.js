d4da8003d05b8717bd8a9954f0c660b1c72cd3ba
U-riani <sandro.papi@yahoo.com>
2024-09-15 18:18:22 +0400

    adjust mail onptions

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -3,8 +3,19 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const emailController = async (req, res) => {
-  const { name, companyName, email, countryCode, phoneNumber, comments } =
-    req.body;
+  const {
+    name,
+    companyName,
+    email,
+    countryCode,
+    phoneNumber,
+    comments,
+    personCompany,
+    panelsPlace,
+    expense,
+    stationPower,
+    stationPrice,
+  } = req.body;
 
   const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -18,19 +29,21 @@ const emailController = async (req, res) => {
     from: email,
     to: "sandropapiashvili@gmail.com",
     subject: "New Email from Website",
-    text: `Name: ${name}\nCompany Name: ${companyName}\nEmail: ${email}\nPhone Number: ${countryCode} ${phoneNumber}\nComments: ${comments}`,
+    text: `Name: ${name}\nCompany Name: ${companyName}\nEmail: ${email}\nPhone Number: ${countryCode} ${phoneNumber}\nComments: ${comments}\nStationInfo: For: - ${personCompany}\n panels place: - ${panelsPlace}\nexpense per month: - ${expense}\nstation Power: - ${stationPower}\nstation Price: - ${stationPrice}`,
+    
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error("Error sending email:", error);
-      res.status(500).json({ message: "Error sending email", error: error.toString() });
+      res
+        .status(500)
+        .json({ message: "Error sending email", error: error.toString() });
     } else {
       console.log("Email sent:", info.response);
-      res.status(200).send("Email sent successfully");
+      res.status(200).send(`${mailOptions}`);
     }
   });
-  
 };
 
 const testGetemail = async (req, res) => {
