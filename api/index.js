ef2a140ce77d5a5fb149352fc9bcbd7e47219e1e
U-riani <sandro.papi@yahoo.com>
2024-09-18 22:22:39 +0400

    adjust origins

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 // Enable CORS
 app.use(
   cors({
-    origin: ["https://oli.wic.temporary.site", 'http://localhost:3000'], // Replace with your frontend URL
+    origin: ["https://ohmenergy.ge/","https://oli.wic.temporary.site", 'http://localhost:3000'], // Replace with your frontend URL
   })
 );
 
